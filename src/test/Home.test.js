1c47c1074ccbb1580e3e5f59ffd6b7453290bb93
getssh <gtamrat33@gmail.com>
2023-04-25 10:21:34 +0300

    Update Home test file to test search fucntion and rendring correct Elements

--- src/test/Home.test.js
+++ src/test/Home.test.js
@@ -1,22 +1,89 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+// Home.test.js
+
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import Home from '../components/Home';
-import store from '../redux/store';
+
+const mockStore = configureStore([thunk]);
 
 describe('Home', () => {
-  it('renders the Home component', async () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      trending: {
+        isLoading: false,
+        trending: [
+          {
+            id: 'bitcoin',
+            coin_id: 'bitcoin',
+            name: 'Bitcoin',
+            market_cap_rank: 1,
+            img: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579',
+          },
+          {
+            id: 'ethereum',
+            coin_id: 'ethereum',
+            name: 'Ethereum',
+            market_cap_rank: 2,
+            img: 'https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880',
+          },
+        ],
+        error: null,
+      },
+    });
+  });
+
+  it('renders the search input', () => {
     render(
       <Provider store={store}>
-        <Home />
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
       </Provider>
     );
 
-    // Wait for the fetchCoins Promise to resolve
+    expect(screen.getByPlaceholderText('Search')).toBeInTheDocument();
+  });
+
+  it('filters the coins based on the search input', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const searchInput = screen.getByPlaceholderText('Search');
+    fireEvent.change(searchInput, { target: { value: 'Bit' } });
+
+    const bitcoinCoin = screen.getByText('Bitcoin');
+    expect(bitcoinCoin).toBeInTheDocument();
+
+    const ethereumCoin = screen.queryByText('Ethereum');
+    expect(ethereumCoin).not.toBeInTheDocument();
+  });
+
+  it('renders the Home component', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      </Provider>
+    );
+
     await screen.findByText('STATS BY TRENDING COIN');
+    await screen.findByText('TRENDING COINS');
+    await screen.findByPlaceholderText('Search');
 
     expect(screen.getByText('STATS BY TRENDING COIN')).toBeInTheDocument();
-    expect(screen.getByText('STATS BY TRENDING COIN')).toBeInTheDocument();
-    expect(screen.getByText('STATS BY TRENDING COIN')).toBeInTheDocument();
+    expect(screen.getByText('TRENDING COINS')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Search')).toBeInTheDocument();
   });
 });
+
